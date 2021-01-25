c8fae50e2fd6252398035da367aaf5432e4c342e
Lily Yang <neixingren@gmail.com>
2021-01-25 21:09:59 +1100

    bottom navigation style

--- navigations/BottomNavigation.tsx
+++ navigations/BottomNavigation.tsx
@@ -2,14 +2,59 @@ import * as React from "react";
 import { Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import COLOR from "../constants/colors";
 import TripListScreen from "../screens/trip/TripListScreen";
+import TripItemScreen from "../screens/trip/TripItemScreen";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 const BottomTab = createBottomTabNavigator();
 
+const tripListScreenOptions = ({ route }: any) => ({
+  tabBarIcon: ({ focused, color, size }: any) => {
+    return <MaterialCommunityIcons name="map-clock" size={30} color={color} />;
+  },
+});
+
+const tripItemScreenOptions = ({ route }: any) => ({
+  tabBarIcon: ({ focused, color, size }: any) => {
+    return (
+      <MaterialCommunityIcons name="bag-personal" size={30} color={color} />
+    );
+  },
+});
+
+const contactsScreenOptions = ({ route }: any) => ({
+  tabBarIcon: ({ focused, color, size }: any) => {
+    return <MaterialCommunityIcons name="contacts" size={30} color={color} />;
+  },
+});
+
 const BottomNavigation = () => {
   return (
-    <BottomTab.Navigator>
-      <BottomTab.Screen name="tripList" component={TripListScreen} />
+    <BottomTab.Navigator
+      tabBarOptions={{
+        activeBackgroundColor: COLOR.flatLight,
+        activeTintColor: COLOR.boldLight,
+        inactiveBackgroundColor: COLOR.flatLight,
+        inactiveTintColor: COLOR.flatGreyer,
+        showLabel: false,
+      }}
+    >
+      <BottomTab.Screen
+        options={tripListScreenOptions}
+        name="tripList"
+        component={TripListScreen}
+      />
+      <BottomTab.Screen
+        options={tripItemScreenOptions}
+        name="tripItem"
+        component={TripItemScreen}
+      />
+      <BottomTab.Screen
+        options={contactsScreenOptions}
+        name="contacts"
+        component={TripItemScreen}
+      />
     </BottomTab.Navigator>
   );
 };
---
+++ screens/trip/NewTripScreen.tsx
@@ -0,0 +1,22 @@
+import React, { useEffect } from "react";
+import { View, StyleSheet, Dimensions, FlatList, Image } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import FONT from "../../constants/fonts";
+import COLOR from "../../constants/colors";
+import AppText from "../../components/AppText";
+import { Trip } from "../../models/trip";
+import { useDispatch, useSelector } from "react-redux";
+import * as TripActions from "../../stores/actions/trips.action";
+import { User } from "../../models/user";
+import Environment from "./../../environment";
+import { TouchableOpacity } from "react-native-gesture-handler";
+
+const NewTripScreen = (props: any) => {
+  return (
+    <View>
+      <AppText>new trip</AppText>
+    </View>
+  );
+};
+
+export default NewTripScreen;
